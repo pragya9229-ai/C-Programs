1838434a656f508ba5cae4b749d7780dc431ba49
Pragya <pragya9229@gmail.com>
2026-01-30 11:42:27 +0530

    Add files via upload

---
+++ download.c
@@ -0,0 +1,74 @@
+// #include <stdio.h>
+// int main()
+// {
+//     int a;
+//     printf("enter a number: ");
+//     scanf("%d", &a);
+//     printf("%d" ,a);
+//     return 0;
+// }
+
+// QUES3
+// #include<stdio.h>
+// int main(){
+//     int a,b;
+//     printf("Enter a number: \n");
+//     scanf("%d",&a);
+//     printf("Enter a number: \n");
+//     scanf("%d" ,&b);
+//     int sum=a+b;
+//     printf("The sum of the number: %d ",sum);
+//     return 0;
+// }
+
+// volume of sphere
+// #include<stdio.h>
+// int main(){
+//     int r;
+//     printf("enter a number: \n");
+//     scanf("%d", &r);
+//     float vol= (4.0/3.0)*3.14*r*r*r;
+//     printf("the volume of sphere is: %f", vol);
+//     return 0;
+// }
+
+// perimeter of rectangle
+// #include<stdio.h>
+// int main(){
+//     int a,b;
+//     printf("enter a number: \n");
+//     scanf("%d" ,&a);
+//     printf("enter a number: \n");
+//     scanf("%d" ,&b);
+//     float peri= 2*(a+b);
+//     printf("the perimeter is: %f",peri);
+//     return 0;
+
+// }
+
+// centigrate to fahrenheit
+// #include<stdio.h>
+// int main(){
+//     float c;
+//     printf("enter a number: \n");
+//     scanf("%f",&c);
+//     float con=(9.0/5.0)*c+32;
+//     printf("the conversion is:  %f",con);
+//     return 0;
+// }
+
+// DOB,NAME,PhoneNO
+#include<stdio.h>
+int main(){
+    char name[40];
+    printf("enter your name: \n");
+    scanf("%s",&name);
+    char month[20];
+    printf("enter the month: \n");
+    scanf("s",&month);
+    int date,year;
+    printf("Enter the date: \n");
+    scanf("%d",&date);
+    printf("enter the year: \n");
+    scanf("%d",&year)
+}
